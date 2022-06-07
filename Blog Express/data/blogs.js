4e45f69b13ff4a789cee18456d6b711992737bf5
Jay <jay.sanjay.karia@gmail.com>
2022-06-07 22:59:22 +0530

    Add files via upload

---
+++ Blog Express/data/blogs.js
@@ -0,0 +1,24 @@
+blogs= [
+        {
+            "title": "HTML an Introduction",
+            "content": "This is the content --html"
+        },
+        {
+            "title": "CSS an Introduction",
+            "content": "This is the content --css"
+        },
+        {
+            "title": "JS an Introduction",
+            "content": "This is the content --js"
+        },
+        {
+            "title": "Node.js an Introduction",
+            "content": "This is the content --node"
+        },
+        {
+            "title": "Express.js an Introduction",
+            "content": "This is the content --express"
+        }
+    ]
+
+module.exports = blogs
